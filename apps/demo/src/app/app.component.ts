53c91b9796f5278682449b0b72f8eeec12b077a1
Ajit Panigrahi <ajitzero@users.noreply.github.com>
2023-06-18 22:59:17 +0530

    :sparkles: Added Angular Directive
    Currently, options can be passed on a per directive usage basis, inline within the template.

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,14 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgAutoAnimateDirective } from 'ng-auto-animate';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [CommonModule, RouterModule, NgAutoAnimateDirective],
   selector: 'ng-auto-animate-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <main class="container">
+      <hgroup>
+        <h2>NgAutoAnimateDirective</h2>
+        <h3>Wrapper around FormKit's Auto Animate library</h3>
+      </hgroup>
+
+      <article>
+        <button (click)="show = !show">Toggle</button>
+        <button (click)="shuffle()">Shuffle</button>
+        <footer [auto-animate]="{ duration: 250 }">
+          <h3>Footer content</h3>
+          <div *ngIf="show" auto-animate>
+            <p *ngFor="let paragraph of paragraphs">{{ paragraph }}</p>
+          </div>
+        </footer>
+      </article>
+    </main>
+  `,
 })
 export class AppComponent {
-  title = 'demo';
+  title = 'Demo';
+  show = false;
+
+  paragraphs = [
+    '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    '4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    '5. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    '6. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    '7. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  ];
+
+  shuffle() {
+    if (this.show) {
+      this.paragraphs = this.paragraphs.sort(() => Math.random() - 0.5);
+    }
+  }
 }
--- libs/ng-auto-animate/src/lib/ng-auto-animate.directive.ts
+++ libs/ng-auto-animate/src/lib/ng-auto-animate.directive.ts
@@ -1,9 +1,25 @@
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, inject } from '@angular/core';
+import autoAnimate, { AutoAnimateOptions } from '@formkit/auto-animate';
+
 
 @Directive({
   selector: '[auto-animate]',
   standalone: true,
 })
-export class NgAutoAnimateDirective {
-  // TODO
+export class NgAutoAnimateDirective implements AfterViewInit {
+  private el = inject(ElementRef);
+
+  @Input('auto-animate')
+  set options(_options: Partial<AutoAnimateOptions> | '') {
+    this._options = typeof _options === 'string' ? {} : _options;
+  }
+  get options() {
+    return this._options;
+  }
+  private _options: Partial<AutoAnimateOptions> = {};
+
+  ngAfterViewInit(): void {
+    this.options = typeof this.options === 'string' ? {} : this.options;
+    autoAnimate(this.el.nativeElement, { ...this.options });
+  }
 }
