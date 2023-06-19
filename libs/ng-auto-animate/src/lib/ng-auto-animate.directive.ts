319640044fd7e4465b78855c391938bee221990f
Ajit Panigrahi <ajitzero@gmail.com>
2023-06-19 15:56:26 +0000

    :sparkles: Added support for plugins

--- libs/ng-auto-animate/src/lib/ng-auto-animate.directive.ts
+++ libs/ng-auto-animate/src/lib/ng-auto-animate.directive.ts
@@ -1,8 +1,11 @@
 import { AfterViewInit, Directive, ElementRef, InjectionToken, Input, inject } from '@angular/core';
-import autoAnimate, { AutoAnimateOptions } from '@formkit/auto-animate';
+import autoAnimate, { AutoAnimateOptions, AutoAnimationPlugin } from '@formkit/auto-animate';
 
-export const GLOBAL_AUTO_ANIMATE_OPTIONS = new InjectionToken<Partial<AutoAnimateOptions>>(
-  'Global AutoAnimate Options', {
+const isPlugin = (config: Partial<AutoAnimateOptions> | AutoAnimationPlugin) =>
+  typeof config === 'function';
+
+export const GLOBAL_AUTO_ANIMATE_OPTIONS = new InjectionToken<Partial<AutoAnimateOptions> | AutoAnimationPlugin>(
+  'Global AutoAnimate Options or Plugin', {
   factory: () => ({}),
 });
 
@@ -15,16 +18,29 @@ export class NgAutoAnimateDirective implements AfterViewInit {
   private globalOptions = inject(GLOBAL_AUTO_ANIMATE_OPTIONS);
 
   @Input('auto-animate')
-  set explicitOptions(_options: Partial<AutoAnimateOptions> | string) {
-    this._options = {
-      ...this.globalOptions,
-      ...(typeof _options === 'string' ? {} : _options),
-    };
+  set explicitOptions(_options: Partial<AutoAnimateOptions> | AutoAnimationPlugin | string) {
+    if (typeof _options === 'string') {
+      // Default case, when no options or plugin is passed
+      this._options = this.globalOptions;
+    } else {
+      // When either some options or a plugin is passed
+      if (isPlugin(this.globalOptions) || isPlugin(_options)) {
+        // A plugin must replace any previously set options or plugin. 
+        // A plugin must be replaced by options or another plugin.
+        this._options = _options;
+      } else {
+        // When plugins are not involved
+        this._options = {
+          ...this.globalOptions,
+          ..._options,
+        };
+      }
+    }
   }
   get options() {
     return this._options;
   }
-  private _options: Partial<AutoAnimateOptions> = {};
+  private _options: Partial<AutoAnimateOptions> | AutoAnimationPlugin = {};
 
   ngAfterViewInit(): void {
     autoAnimate(this.el.nativeElement, this.options);
