db6e0eb2919fbb22cd03b4a6a00b3b9da5951997
Ajit Panigrahi <ajitzero@gmail.com>
2026-02-09 21:12:49 +0530

    build: upgrade workspace to Nx 22

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { GLOBAL_AUTO_ANIMATE_OPTIONS } from 'ng-auto-animate';
 import { appRoutes } from './app.routes';
@@ -7,6 +7,6 @@ export const appConfig: ApplicationConfig = {
 	providers: [
 		{ provide: GLOBAL_AUTO_ANIMATE_OPTIONS, useValue: { duration: 1000 } },
 		provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-		provideExperimentalZonelessChangeDetection(),
+		provideZonelessChangeDetection(),
 	],
 };
