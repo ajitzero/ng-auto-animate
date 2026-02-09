b1b1c0ee9eec597a72f49dabbd187da50e441f36
Ajit Panigrahi <ajitzero@gmail.com>
2026-02-09 20:59:23 +0530

    build: upgrade workspace to Nx 21

---
+++ apps/demo-e2e/eslint.config.mjs
@@ -0,0 +1,30 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import baseConfig from '../../eslint.config.mjs';
+
+const compat = new FlatCompat({
+	baseDirectory: dirname(fileURLToPath(import.meta.url)),
+	recommendedConfig: js.configs.recommended,
+});
+
+export default [
+	{
+		ignores: [
+			'**/dist',
+		],
+	},
+	...baseConfig,
+	...compat.extends('plugin:cypress/recommended'),
+	{
+		files: [
+			'**/*.ts',
+			'**/*.tsx',
+			'**/*.js',
+			'**/*.jsx',
+		],
+		// Override or add rules here
+		rules: {},
+	},
+];
